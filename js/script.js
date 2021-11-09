92f3c3c17c71684a1ce1f609a560f6560e7bd4e2
Jose Vilchez <jjva90@gmail.com>
2021-11-08 23:05:22 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log("Hey, didn't see you there 👀");
+
+const colors = {
+  "circuit-board-green": "#193923",
+  "cyber-space-orange": "#c23b20",
+  "floppy-yellow": "#b16c24",
+  "graphic-green": "#172526",
+  "green.com": "#357739",
+  "infra-red": "#c43423",
+  "megabyte-blue": "#201e2c",
+  "megahertz-maroon": "#621b23",
+  "online-orange": "#d65128",
+  "point-&-click-orange": "#4c9830",
+  "plug-&-play-pink": "#c33429",
+  "transistor-yellow": "#dc6e2e",
+  "ultra-violet": "#3d0916",
+  "web-surfing-blue": "#192263",
+  "world-wide-yellow": "#c1a234",
+  "www.purple": "#282329",
+};
+
+console.log(colors);
